cd3237d19c7937c7fa2ebedcfc984b6217eebc4a
Paris Polyzos <ppolyzos@gmail.com>
2023-02-01 16:39:03 +0100

    chore: plugin setup and upgrade yarn

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,20 +5,19 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'prettier'],
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'sonar-scanner.ts'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'no-return-await': 'off',
+    '@typescript-eslint/return-await': 'error',
   },
 };
